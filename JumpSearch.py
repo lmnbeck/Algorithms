15e58e0a4ba246da5b18bbe64ef55313d107f4fb
lmnbeck <maniagea09@gmail.com>
2018-03-22 10:24:58 +0800

    finished jump search
    finished jump search

--- JumpSearch.py
+++ JumpSearch.py
@@ -1,21 +1,22 @@
 import * from linearSearch
+import math
 
-def JumpSearch(arr,blockSize,value):
-    blockID = len(arr)//blockSize
-
+def JumpSearch(arr,value):
+    step = int(math.sqrt(len(arr)))
+    blockID = step
     while blockID <= len(arr):
         print(blockID)
         if arr[blockID] == value:
             return blockID
         elif arr[blockID] > value:
-            return linearSearch(arr,blockID-blockSize,value)
+            return linearSearch(arr,blockID-step,value)
         else:
-            blockID += blockSize
+            blockID += step
 
     return -1
 
 #Test Function
 arr = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610]
 x = 55
-result = JumpSearch(arr,4,x)
-print(result)
+result = JumpSearch(arr,x)
+print("result = ",result)
